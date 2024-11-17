55b2fd952cda72c11917c8c0a256f95c18bfcabc
Jack <khonhong46632@gmail.com>
2024-11-17 23:06:07 +0100

    dashboard structure

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
-    <div>Dashboard
+    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows'>
+      <div className='row-span-3 xl:row-span-6 bg-gray-500' />
+      <div className='row-span-3 xl:row-span-6 bg-gray-500' />
 
+      {/* This section takes 2 columns if screen size is larger than 768px (md) */}
+      <div className='row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500' />
+      
+      <div className='row-span-3 bg-gray-500' />
+      <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
+      <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
+      <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,8 +1,59 @@
-import {createApi, fetchBaseQuery  } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Product {
+  productId: string;
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
+export interface SalesSummary {
+  salesSummaryId: string;
+  totalValue: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface PurchaseSummary {
+  purchaseSummaryId: string;
+  totalPurchased: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface ExpenseSummary {
+  expenseSummaryId: string;
+  totalExpenses: number;
+  date: string;
+}
+
+export interface ExpenseByCategory {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: number;
+  date: string;
+}
+export interface DashboardMetrics {
+  popularProducts: Product[];
+  salesSummary: SalesSummary[];
+  purchaseSummary: PurchaseSummary[];
+  expenseSummary: ExpenseSummary[];
+  expenseByCategory: ExpenseByCategory[];
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-    reducerPath: "api",
-    tagTypes: [], // used to configure caching and invalidation of cached data
-    endpoints: (builder) => ({}),
-})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: 'api',
+  tagTypes: ['DashboardMetrics'], // used to configure caching and invalidation of cached data
+  endpoints: (builder) => ({
+    getDashboardMetrics: builder.query<DashboardMetrics, void>({
+      query: () => '/dashboard',
+      // If the "DashboardMetrics" tag is invalidated,
+      // RTK Query will refetch this query's data.
+      providesTags: ['DashboardMetrics'],
+    }),
+  }),
+});
+ 
+export const { useGetDashboardMetricsQuery } = api;

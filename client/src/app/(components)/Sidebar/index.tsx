834fccf6b6b4320f60137e1d3d14c00f4fe9167a
Jack <khonhong46632@gmail.com>
2025-01-05 23:51:52 +0100

    minor change on the sidebar footer

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -144,7 +144,7 @@ const Sidebar = () => {
 
       {/* FOOTER */}
       <div className={`${isSidebarCollapsed ? 'hidden' : 'block'} mb-10`}>
-        <p className='text-center text-xs text-gray-500'>&copy; 2024</p>
+        <p className='text-center text-xs text-gray-500'>&copy; 2024 INVSTOCK</p>
       </div>
     </div>
   );
